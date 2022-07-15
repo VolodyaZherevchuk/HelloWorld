de28765f1034431160ad46cb70c2f640495ba7b6
VolodyaZherevchuk <volodya.zherevchuk@gmail.com>
2022-07-15 11:05:07 +0300

    initial commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,18 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
-        
+        byte happyValue = 8;
+        short cold = -50;
+        int globalAnswer = 42;
+        int randomAnswer;
+        randomAnswer = 700;
+        long longerThanLife = 223372036869;
+
+        //Example output
+        System.out.println("happyValue = " + happyValue);
+        System.out.println("cold = " + cold);
+        System.out.println("globalAnswer = " + globalAnswer);
+        System.out.println("randomAnswer = " + randomAnswer);
+        System.out.println("longerThanLife = " + longerThanLife);
     }
 }
